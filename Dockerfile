b52ca4c6bbf6d4e546f52fd81c5e946e9fc7748b
chiperi <kiperelena6@gmail.com>
2026-02-24 13:38:16 +0100

    done

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,15 @@
+FROM golang:1.22-alpine AS build
+WORKDIR /src
+COPY go.mod go.sum ./
+RUN go mod download
+COPY . .
+RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o /out/webapp ./main.go
 
+FROM gcr.io/distroless/static:nonroot
+WORKDIR /app
+COPY --from=build /out/webapp /app/webapp
+COPY --from=build /src/public /app/public
+ENV PORT=8080
+EXPOSE 8080
+USER nonroot:nonroot
+ENTRYPOINT ["/app/webapp"]
--- main.go
+++ main.go
@@ -1,18 +1,35 @@
 package main
 
 import (
-    "github.com/labstack/echo/v4"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"webapp/pkg/config"
 )
 
 func main() {
 	port := config.GetEnv("PORT", "3000")
 
-    e := echo.New()
-    e.Static("/", "public")
-    
-    e.GET("/", func(c echo.Context) error {
-        return c.File("public/views/webapp.html")
-    })
-    e.Start(":" + port)
+	e := echo.New()
+
+	logDir := "/tmp/webapp-logs"
+	_ = os.MkdirAll(logDir, 0755)
+	logFile, err := os.OpenFile(filepath.Join(logDir, "app.log"), os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err == nil {
+		mw := io.MultiWriter(os.Stdout, logFile)
+		e.Logger.SetOutput(mw)
+		e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{Output: mw}))
+	} else {
+		e.Use(middleware.Logger())
+	}
+	e.Use(middleware.Recover())
+	e.Static("/", "public")
+
+	e.GET("/", func(c echo.Context) error {
+		return c.File("public/views/webapp.html")
+	})
+	e.Start(":" + port)
 }
